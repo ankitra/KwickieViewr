d4d398b63b7cca7ce439d2cb62e82d524578e7a0
Guest Users <guestusers@ajays-imac.local>
2016-02-28 11:07:45 +0530

    Initial Commit

---
+++ KwikieViewR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KwikieViewR
+//
+//  Created by Guest Users on 28/02/16.
+//  Copyright © 2016 Ankit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KwikieViewR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KwikieViewR
+//
+//  Created by Guest Users on 28/02/16.
+//  Copyright © 2016 Ankit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
